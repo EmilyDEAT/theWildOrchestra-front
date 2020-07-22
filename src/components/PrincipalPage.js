1053e7926d5421bf295151eb87754b32b2737383
Emily Déat <emily.deat@gmail.com>
2020-07-22 20:53:09 +0200

    Create Header

---
+++ src/components/PrincipalPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Header from './Header'
+
+import './PrincipalPage.css'
+
+const PrincipalPage = () => {
+  return (
+    <div className='PrincipalPage-container'>
+      <Header />
+    </div>
+  )
+}
+
+export default PrincipalPage
