12ea3d25aab48efe00ca298e8bd569d90dec7088
Emily Déat <emily.deat@gmail.com>
2020-07-22 21:17:43 +0200

    Add Presentation

--- src/components/PrincipalPage.js
+++ src/components/PrincipalPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Header from './Header'
+import Presentation from './Presentation'
 
 import './PrincipalPage.css'
 
@@ -8,6 +9,7 @@ const PrincipalPage = () => {
   return (
     <div className='PrincipalPage-container'>
       <Header />
+      <Presentation />
     </div>
   )
 }
